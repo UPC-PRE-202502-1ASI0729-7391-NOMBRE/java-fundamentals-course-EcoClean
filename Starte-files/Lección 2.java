a18e7dd450782ec43916963af0ceb1b5fb60a2ab
DiegoAndresAvalos <u202313922@upc.edu.pe>
2025-11-13 00:06:36 -0500

    Update examples in Lección 2.java

---
+++ Starte-files/Lección 2.java
@@ -0,0 +1,38 @@
+####### Example 1 #######
+
+public class Main {
+    public static void main(String[] args) {
+        int edad = 15;
+        System.out.println("Mi edad es: " + edad);
+    }
+}
+
+####### Example 2 #######
+
+public class Main {
+    public static void main(String[] args) {
+       int numero1 = 10;
+       int numero2 = 5;
+       int suma = numero1 + numero2;
+       System.out.println("La suma es: " + suma);
+    }
+}
+  
+####### Example 3 #######
+
+public class Main {
+    public static void main(String[] args) {
+       String nombre = "Ana";
+       System.out.println("Hola, " + nombre + "!");
+    }
+} 
+
+####### Example 4 #######
+
+public class Main {
+    public static void main(String[] args) {
+      int edad = 16;
+      int añosFaltantes = 18 - edad;
+      System.out.println("Faltan " + añosFaltantes + " años para ser mayor de edad.");
+    }
+} 
