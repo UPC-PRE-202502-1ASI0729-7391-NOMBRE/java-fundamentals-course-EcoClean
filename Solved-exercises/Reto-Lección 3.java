3602225516105d13c522d6856409134b0fdfb402
DiegoAndresAvalos <u202313922@upc.edu.pe>
2025-11-15 16:13:54 -0500

    Update Reto-Lección 3.java

--- Solved-exercises/Reto-Lección 3.java
+++ Solved-exercises/Reto-Lección 3.java
@@ -1,1 +1,15 @@
+///Como práctica, te reto a crear un programa que muestre los números del 1 al 10, y luego diga si cada número es par o impar.
 
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " es PAR");
+            } else {
+                System.out.println(i + " es IMPAR");
+            }
+        }
+
+    }
+}
