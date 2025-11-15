ee43248087396f54d210d92bd6c7ad892f0bc567
AndS56 <andree567890@gmail.com>
2025-11-15 18:04:03 -0500

    Create Lección 6.java

---
+++ Starte-files/Lección 6.java
@@ -0,0 +1,17 @@
+public class ErroresComunes {
+    public static void main(String[] args) {
+        // Error 1: Falta punto y coma
+        // System.out.println("Hola mundo")
+
+        // Error 2: Nombre de variable incorrecto
+        // int edad = 18;
+        // System.out.println(Edad); // Error: Edad no existe
+
+        // Error 3: Falta llave de cierre
+        // if (edad >= 18) {
+        //     System.out.println("Eres adulto");
+
+        // Error 4: Error lógico (división entre cero)
+        // int resultado = 10 / 0; // Provoca ArithmeticException
+    }
+}
